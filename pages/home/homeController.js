4a2a50bcb098c01415bf243c1d64821faf9ffc56
Maor Elfassy <maorelf@post.bgu.ac.il>
2019-06-13 20:07:57 +0300

    yoao

--- pages/home/homeController.js
+++ pages/home/homeController.js
@@ -1,10 +1,19 @@
 // about controller
 angular.module("myApp")
-.controller("homeController", function ($scope, $http) {
-    $http({
-        method : "GET",
-        url : "http://localhost:5000/private/getTwoPopularInterestPoints"
-    }).then(function mySuccess(response) {
+.controller("homeController", function ($scope, $http, $rootScope) {
+    console.log("maor");
+
+
+    var req = {
+        method: 'GET',
+        url: 'http://localhost:5000/private/getTwoPopularInterestPoints',
+        headers: {
+            'content-type': 'application/json',
+            'x-auth-token': $rootScope.myToken
+        }
+    };
+    $http(req)
+    .then(function mySuccess(response) {
         var results = response.data;
         $scope.rec_atractions = response.data;
     }, function myError(response) {
