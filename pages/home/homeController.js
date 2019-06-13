7e6c2c14a9bffa61d18213125c0d5553d3a18796
Maor Elfassy <maorelf@post.bgu.ac.il>
2019-06-13 23:18:38 +0300

    yoao

--- pages/home/homeController.js
+++ pages/home/homeController.js
@@ -1,7 +1,6 @@
 // about controller
 angular.module("myApp")
 .controller("homeController", function ($scope, $http, $rootScope) {
-    console.log("maor");
 
 
     var req = {
@@ -18,5 +17,25 @@ angular.module("myApp")
         $scope.rec_atractions = response.data;
     }, function myError(response) {
         $scope.myWelcome = response.statusText;
-    })
+    });
+    saved();
+    function saved() {
+        console.log("maor434432143223");
+        var req = {
+            method: 'GET',
+            url: 'private/getLastTwoSavedInterestPoints',
+            headers: {
+                'content-type': 'application/json',
+                'x-auth-token': $rootScope.myToken
+            }
+        };
+        console.log("maor434432143223");
+        $http(req).
+        then(function mySuccess(response) {
+            var results = response.data;
+            $scope.saved_attractions = results;
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        })
+    }
 });
